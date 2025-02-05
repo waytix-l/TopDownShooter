9095775610bfa52c31e03f6008d99f07c639b60b
lucas-sv <lucas.savy@ynov.com>
2025-02-05 16:45:43 +0100

    debut

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Input.GetKey
+    }
+
+    
+}
